3230aa51b4714728a6f5e929a1321b1957002073
catalyyst <catalyyst216@gmail.com>
2023-08-04 08:03:19 +0200

    added fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,29 @@
+fish_add_path ~/.local/bin/
+pfetch
+
+export EDITOR="helix"
+
+if status is-interactive
+    alias dotfiles="/usr/bin/env git --git-dir=$HOME/git/dotfiles/.git --work-tree=$HOME/git/dotfiles"
+    alias da="dotfiles add"
+	alias dc="dotfiles commit -m"
+	alias dp="dotfiles push"
+
+	alias hx="helix"
+    alias t="touch"
+end
+
+set SUDO_EDITOR /usr/bin/helix
+export SUDO_EDITOR
+
+function ls
+    command exa --all --long --group-directories-first --icons $argv
+end
+
+function k --wraps=kubecolor
+    kubecolor $argv
+end
+
+function sue
+    command sudoedit $argv
+end
